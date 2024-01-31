ab35a90a3dd6ab69a7e3226d68044f8886e64643
Alishan Ladhani <git@alad.ca>
2024-01-30 19:25:38 -0500

    Make output filename configurable

--- main.go
+++ main.go
@@ -3,23 +3,22 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/aladh/ip_change_notifier/webhook"
 	"log"
 	"net"
 	"os"
-
-	"github.com/aladh/ip_change_notifier/webhook"
 )
-
-const outputFilename = "ip.txt"
 
 var domain string
 var previousIP string
 var webhookURL string
+var outputFilename string
 
 func init() {
 	flag.StringVar(&domain, "d", "", "The domain to look up")
 	flag.StringVar(&previousIP, "p", "", "The previous IP address")
 	flag.StringVar(&webhookURL, "w", "", "A URL to send webhooks when IP changes")
+	flag.StringVar(&outputFilename, "o", "", "The file to write the IP address to")
 	flag.Parse()
 }
 
@@ -49,11 +48,13 @@ func main() {
 		log.Fatalln("Error sending webhook:", err)
 	}
 
-	file, err := os.Create(outputFilename)
-	if err != nil {
-		log.Fatalln("Error creating file:", err)
-	}
-	defer file.Close()
+	if outputFilename != "" {
+		file, err := os.Create(outputFilename)
+		if err != nil {
+			log.Fatalln("Error creating file:", err)
+		}
+		defer file.Close()
 
-	_, err = file.WriteString(ipString)
+		_, err = file.WriteString(ipString)
+	}
 }
